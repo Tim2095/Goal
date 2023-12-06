10fcc993898f5ed4922a54da6aad4b1a3b6bd470
Timur <timur.mosenkov@gmail.com>
2023-12-06 20:36:22 +0200

    finish header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import goalLogo from '../assets/goal.jpg'
+
+const Header = () => {
+  return (
+    <header>
+      <img src={goalLogo} alt="header logo" />
+    </header>
+  )
+}
+
+export default Header
